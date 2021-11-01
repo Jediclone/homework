d8ed0ed8ea4a4299bfc27300a69a530d00e5d2da
Jediclone <lukyanoff.an@yandex.ru>
2021-11-01 19:30:40 +0300

    fix

--- Lesson4/tabs/main.js
+++ Lesson4/tabs/main.js
@@ -6,7 +6,7 @@ const changeClass = el => {
     for (let i = 0; i < tabs.children.length; i++) {
         tabs.children[i].classList.remove('active'); //таким образом удалил active у всех классов в div tabs в html
     }
-    el.classList.add('active'); //добавилю active, куда кликну 
+    el.classList.add('active'); //добавлю active, куда кликну 
 }
 
 tabs.addEventListener('click', e => {
@@ -20,21 +20,24 @@ tabs.addEventListener('click', e => {
     }
 })
 
+const tabsDop = document.getElementById('tabs-dop');
+const contentDop = document.querySelectorAll('.content-dop');
 
-const changeClass = el => {
-    for (let i = 0; i < contents.children.length; i++) {
-        contents.children[i].classList.remove('active'); //таким образом удалил active у всех классов в div tabs в html
+//console.log(tabs.children); //можно смотреть детей родительского элемента
+const changeClassDop = el => {
+    for (let i = 0; i < tabsDop.children.length; i++) {
+        tabsDop.children[i].classList.remove('active'); //таким образом удалил active у всех классов в div tabs в html
     }
-    el.classList.add('active'); //добавилю active, куда кликну 
+    el.classList.add('active'); //добавлю active, куда кликну 
 }
 
-tabs.addEventListener('click', e => {
-    const currTab = e.target.dataset.btn;
-    changeClass(e.target);
-    for (let i = 0; i < content.length; i++) {
-        content[i].classList.remove('active');
-        if (content[i].dataset.content == currTab) {
-            content[i].classList.add('active');
+tabsDop.addEventListener('click', e => {
+    const currTabDop = e.target.dataset.btn;
+    changeClassDop(e.target);
+    for (let i = 0; i < contentDop.length; i++) {
+        contentDop[i].classList.remove('active');
+        if (contentDop[i].dataset.content_dop == currTabDop) {
+            contentDop[i].classList.add('active');
         }
     }
 })
